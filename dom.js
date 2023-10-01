ca43492c202b173fa6580d3cd5b2cdff8a5178d0
nilesh0154 <malgenilesh007@gmail.com>
2023-10-01 12:49:36 +0530

    changes

--- dom.js
+++ dom.js
@@ -1,6 +1,19 @@
-var items = documents.getElementsByClassName('list-group-item');
 
-console.log(items);
+// //getelementsbyclassname
 
-console.log(items[2]);
-items[2].style.backgroundcolor = 'green'; 
+// var items = documents.getElementsByClassName('list-group-item');
+
+// console.log(items);
+
+// console.log(items[2]);
+// items[2].style.backgroundcolor = 'green';
+
+//getelementsbytagname
+
+var li = documents.getElementsByTagName('li');
+
+console.log(li);
+
+console.log(li[2]);
+li[2].style.backgroundcolor = 'green';
+
