61177102e2f558b4769637e9d848d7c8bc590d6b
nilesh0154 <malgenilesh007@gmail.com>
2023-10-01 12:30:05 +0530

    styles

---
+++ dom.js
@@ -0,0 +1,7 @@
+var items = documents.getElementsByClassName('list-group-item');
+
+console.log(items);
+items.style.fontweight = 'bold';
+
+console.log(items[2]);
+items[2].style.backgroundcolor = 'green'; 
